454f7e97988c6ae29a7448f73a19cdde4f6a7d6c
David Barroso <dbarrosop@dravetech.com>
2016-09-13 10:18:32 +0200

    Added tests for the filtered version of get_config

--- napalm_base/test/base.py
+++ napalm_base/test/base.py
@@ -513,3 +513,15 @@ class TestGettersNetworkDriver:
 
         assert isinstance(get_config, dict)
         assert self._test_model(models.config, get_config)
+
+    def test_get_config_filtered(self):
+        """Test get_config method."""
+        for config in ['running', 'startup', 'candidate']:
+            try:
+                get_config = self.device.get_config(retrieve=config)
+            except NotImplementedError:
+                raise SkipTest()
+
+            assert get_config['candidate'] == "" if config != "candidate" else True
+            assert get_config['startup'] == "" if config != "startup" else True
+            assert get_config['running'] == "" if config != "running" else True
