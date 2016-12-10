55ece739a2125e81a659e946ba917079cec53b62
jedelman8 <jedelman8@gmail.com>
2016-12-10 13:11:46 -0800

    added support napalm conf file

--- napalm_base/__init__.py
+++ napalm_base/__init__.py
@@ -19,10 +19,16 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 # Python std lib
+import os
 import sys
 import inspect
 import importlib
 import pkg_resources
+
+try:
+    from configparser import ConfigParser as SafeConfigParser
+except ImportError:
+    from ConfigParser import SafeConfigParser
 
 # Verify Python Version that is running
 try:
@@ -45,9 +51,12 @@ except pkg_resources.DistributionNotFound:
 
 __all__ = [
     'get_network_driver',  # export the function
-    'NetworkDriver'  # also export the base class
+    'NetworkDriver',  # also export the base class
+    'network_device'
 ]
 
+LIB_PATH_ENV_VAR = 'NAPALM_CONF'
+LIB_PATH_DEFAULT = '~/napalm.conf'
 
 def get_network_driver(module_name):
     """
@@ -109,3 +118,120 @@ def get_network_driver(module_name):
     raise ModuleImportError(
         'No class inheriting "napalm_base.base.NetworkDriver" found in "{install_name}".'
         .format(install_name=module_install_name))
+
+def network_device(named_device, filename=None):
+    """
+
+    Instantiate and return an instance of a NetworkDriver
+    If no filename is given the environment variable NAPALM_CONF is checked
+    for a path, and finally then ~/napalm.conf is used.
+
+    Arguments:
+        named_device (string): Name of the device as listed in the napalm configuration file.
+        filename (string): (Optional) Path to napalm configuration file that includes
+            the ``named_device`` argument as section header.
+    Raises:
+        DeviceNameNotFoundError: if the named_device is not found in the
+            napalm configuration file.
+        ConfFileNotFoundError: if no napalm configuration file can be found.
+
+    Example (napalm conf file)::
+
+    .. code-block:: python
+        [nxos:n9k1]
+        username: ntc
+        password: pwd123
+
+        [nxos:n9k2]
+        username: ntc
+        password: pwd123
+
+        [ios:csr1]
+        hostname: 10.1.100.10
+        username: ntc
+        password: ntc123
+
+        [eos:eos-spine1]
+        hostname: 10.1.100.11
+        username: ntc
+        password: ntc123
+
+    Example::
+
+
+    .. code-block:: python
+
+        >>> from napalm import network_device
+        >>> device = network_device('csr1')
+        >>> device
+        <napalm_ios.ios.IOSDriver object at 0x7f8e91392210>
+        >>>
+
+    """
+    config, filename = _get_config_from_file(filename=filename)
+    sections = config.sections()
+
+    """
+    TODO: NEED TO IMPLEMENT ConfFileNotFoundError
+    if not sections:
+        raise ConfFileNotFoundError(filename)
+    """
+
+    for section in sections:
+        if ':' in section:
+            device_type, conn_name = section.split(':')
+            if named_device == conn_name:
+                device_kwargs = dict(config.items(section))
+                if 'hostname' not in device_kwargs:
+                    device_kwargs['hostname'] = named_device
+
+                driver = get_network_driver(device_type)
+                device = _get_device(driver, device_kwargs)
+                device.open()
+                return device
+
+                """
+                TODO: NEED TO IMPLEMENT
+                raise DeviceNotFoundError(name, filename)
+
+                """
+
+def _get_device(driver, device_kwargs):
+
+    core_keys = ['hostname', 'username', 'password', 'timeout']
+    optional_args = dict((k, v) for k, v in device_kwargs.items()
+                         if v is not None and k not in core_keys)
+
+    hostname = device_kwargs['hostname']
+    un = device_kwargs['username']
+    pwd = device_kwargs['password']
+
+    if device_kwargs.get('timeout'):
+        timeout = device_kwargs.get('timeout')
+        if optional_args:
+            device = driver(hostname, un, pwd, timeout=timeout,
+                            optional_args=optional_args)
+        else:
+            device = driver(hostname, un, pwd, timeout=timeout)
+    else:
+        if optional_args:
+            device = driver(hostname, un, pwd,
+                            optional_args=optional_args)
+        else:
+            device = driver(hostname, un, pwd)
+    return device
+
+
+def _get_config_from_file(filename=None):
+
+    if filename is None:
+        if LIB_PATH_ENV_VAR in os.environ:
+            filename = os.path.expanduser(os.environ[LIB_PATH_ENV_VAR])
+        else:
+            filename = os.path.expanduser(LIB_PATH_DEFAULT)
+
+    config = SafeConfigParser()
+    config.read(filename)
+
+    return config, filename
+
