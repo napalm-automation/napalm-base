6b8fe2b04a4d2222f71ea5cea132c89317041c03
Kirk Byers <ktbyers@twb-tech.com>
2017-01-03 21:34:55 -0800

    Correct errors in fix to handle long data type

--- napalm_base/test/helpers.py
+++ napalm_base/test/helpers.py
@@ -13,12 +13,13 @@ def test_model(model, data):
     if not same_keys:
         print("model_keys: {}\ndata_keys: {}".format(sorted(model.keys()), sorted(data.keys())))
 
+    correct_class = True
     for key, instance_class in model.items():
+        correct_class = isinstance(data[key], instance_class)
         # Properly handle PY2 long
-        if isinstance(data[key], long) and isinstance(1, instance_class) and py23_compat.PY2:
-            correct_class = True
-        else:
-            correct_class = isinstance(data[key], instance_class)
+        if py23_compat.PY2:
+            if isinstance(data[key], long) and isinstance(1, instance_class):
+                correct_class = True
         if not correct_class:
             print("key: {}\nmodel_class: {}\ndata_class: {}".format(
                                                     key, instance_class, data[key].__class__))
