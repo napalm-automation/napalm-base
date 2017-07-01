579a7ecdb7c1923de7ed369e9d24216d5aa09005
David Barroso <dbarrosop@dravetech.com>
2017-07-01 14:35:18 +0200

    Implemented generic mock driver

---
+++ napalm_base/mock.py
@@ -0,0 +1,100 @@
+# Copyright 2017 Dravetech AB. All rights reserved.
+#
+# The contents of this file are licensed under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with the
+# License. You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations under
+# the License.
+
+# Python3 support
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from napalm_base.base import NetworkDriver
+import napalm_base.exceptions
+
+import inspect
+import json
+import os
+
+
+def is_mocked_method(method):
+    mocked_methods = ["cli", ]
+    if method.startswith("get_") or method in mocked_methods:
+        return True
+    return False
+
+
+def mocked_method(path, name, count):
+    parent_method = getattr(NetworkDriver, name)
+    parent_method_args = inspect.getargspec(parent_method)
+    modifier = 0 if 'self' not in parent_method_args.args else 1
+
+    def _mocked_method(*args, **kwargs):
+        # Check len(args)
+        if len(args) + len(kwargs) + modifier > len(parent_method_args.args):
+            raise TypeError(
+                "{}: expected at most {} arguments, got {}".format(
+                    name, len(parent_method_args.args), len(args) + modifier))
+
+        # Check kwargs
+        unexpected = [x for x in kwargs if x not in parent_method_args.args]
+        if unexpected:
+            raise TypeError("{} got an unexpected keyword argument '{}'".format(name,
+                                                                                unexpected[0]))
+
+        filename = "{}.{}".format(os.path.join(path, name), count)
+        try:
+            with open(filename) as f:
+                return json.loads(f.read())
+        except IOError:
+            raise NotImplementedError("You can provide mocked data in {}".format(filename))
+
+    return _mocked_method
+
+
+class MockDriver(NetworkDriver):
+
+    def __init__(self, hostname, username, password, timeout=60, optional_args=None):
+        """
+        Supported optional_args:
+            * path(str) - path to where the mocked files are located
+            * profile(list) - List of profiles to assign
+        """
+        self.hostname = hostname
+        self.username = username
+        self.password = password
+        self.path = optional_args["path"]
+        self.profile = optional_args.get("profile", [])
+
+        self.opened = False
+        self.calls = {}
+
+    def _count_calls(self, name):
+        current_count = self.calls.get(name, 0)
+        self.calls[name] = current_count + 1
+        return self.calls[name]
+
+    def open(self):
+        self.opened = True
+
+    def close(self):
+        self.opened = False
+
+    def is_alive(self):
+        return {"is_alive": self.opened}
+
+    def __getattribute__(self, name):
+        if is_mocked_method(name):
+            if not self.opened:
+                raise napalm_base.exceptions.ConnectionClosedException("connection closed")
+            count = self._count_calls(name)
+            return mocked_method(self.path, name, count)
+        else:
+            return object.__getattribute__(self, name)
---
+++ test/unit/TestMockDriver.py
@@ -0,0 +1,88 @@
+"""
+Test base helpers.
+"""
+
+# Python3 support
+from __future__ import print_function
+from __future__ import unicode_literals
+
+
+# NAPALM base
+from napalm_base import get_network_driver
+import napalm_base.exceptions
+
+import pytest
+
+import os
+
+
+BASE_PATH = os.path.dirname(__file__)
+
+
+driver = get_network_driver("mock")
+optional_args = {
+    "path": os.path.join(BASE_PATH, "test_mock_driver"),
+    "profile": ["eos"],
+}
+
+
+class TestMockDriver():
+    """Test Mock Driver."""
+
+    def test_basic(self):
+        d = driver("blah", "bleh", "blih", optional_args=optional_args)
+        assert d.is_alive() == {u'is_alive': False}
+        d.open()
+        assert d.is_alive() == {u'is_alive': True}
+        d.close()
+        assert d.is_alive() == {u'is_alive': False}
+
+        with pytest.raises(napalm_base.exceptions.ConnectionClosedException) as excinfo:
+            d.get_facts()
+        assert "connection closed" in excinfo.value
+
+    def test_context_manager(self):
+        with driver("blah", "bleh", "blih", optional_args=optional_args) as d:
+            assert d.is_alive() == {u'is_alive': True}
+        assert d.is_alive() == {u'is_alive': False}
+
+    def test_mocking_getters(self):
+        d = driver("blah", "bleh", "blih", optional_args=optional_args)
+        d.open()
+        assert d.get_facts()["hostname"] == "localhost"
+        assert d.get_facts()["hostname"] == "changed_hostname"
+        d.close()
+
+    def test_not_mocking_getters(self):
+        d = driver("blah", "bleh", "blih", optional_args=optional_args)
+        d.open()
+
+        with pytest.raises(NotImplementedError) as excinfo:
+            d.get_route_to()
+        expected = "You can provide mocked data in {}/get_route_to.1".format(optional_args["path"])
+        assert expected in excinfo.value
+
+        with pytest.raises(NotImplementedError) as excinfo:
+            d.get_route_to()
+        expected = "You can provide mocked data in {}/get_route_to.2".format(optional_args["path"])
+        assert expected in excinfo.value
+
+        d.close()
+
+    def test_arguments(self):
+        d = driver("blah", "bleh", "blih", optional_args=optional_args)
+        d.open()
+
+        with pytest.raises(TypeError) as excinfo:
+            d.get_route_to(1, 2, 3)
+        assert "get_route_to: expected at most 3 arguments, got 4" in excinfo.value
+
+        with pytest.raises(TypeError) as excinfo:
+            d.get_route_to(1, 1, protocol=2)
+        assert "get_route_to: expected at most 3 arguments, got 3" in excinfo.value
+
+        with pytest.raises(TypeError) as excinfo:
+            d.get_route_to(proto=2)
+        assert "get_route_to got an unexpected keyword argument 'proto'" in excinfo.value
+
+        d.close()
